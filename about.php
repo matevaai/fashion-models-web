e576243721165b76758ab322fdb557d109d9cac8
Mateva <a.mateva@yahoo.com>
2015-09-04 18:44:33 +0300

    Additional pages

---
+++ about.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<?php
+$title = "About";
+$currentPage = "about";
+?>
+<?php include 'lib/head.php'; ?>
+<body role="document">
+	<?php include 'lib/nav.php'; ?>
+
+    <div class="container theme-showcase" role="main">
+		<div class="page-header">
+			<h1>About</h1>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				Under construction
+			</div>
+		</div> <!-- ./row -->
+	</div> <!--./container -->
+</body>
+</html>
---
+++ contact.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<?php
+$title = "Contact";
+$currentPage = "contact";
+?>
+<?php include 'lib/head.php'; ?>
+<body role="document">
+	<?php include 'lib/nav.php'; ?>
+
+    <div class="container theme-showcase" role="main">
+		<div class="page-header">
+			<h1>Contact</h1>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				Under construction
+			</div>
+		</div> <!-- ./row -->
+	</div> <!--./container -->
+</body>
+</html>
